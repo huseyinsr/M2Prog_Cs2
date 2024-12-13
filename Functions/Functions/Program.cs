a1f0fe17e7f5219399559c89fb169455aa6c6dba
husos <husos@HSEYIN>
2024-12-13 09:12:22 +0100

    jkodk

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Functions
+﻿using System.Net.Security;
+
+namespace Functions
 {
     internal class Program
     {
+        internal void Vraag1()
+        {
+            Console.WriteLine("how long do you think....");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+        Function(iets)
+        internal void Run ()
+        {
+            Console.WriteLine("iets");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           Program program = new Program();
+            program Run();
         }
+        void Run()
+        { Console.WriteLine()
+                }
+
     }
 }
